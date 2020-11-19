56ae2de1ba95ed1835c42673df87c0724c944781
Noor-Muhammadd <58544993+Noor-Muhammadd@users.noreply.github.com>
2020-11-19 20:39:41 +0600

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ import './Footer.css';
 const Footer = () => {
     return (
     
-    <div class="footer-dark">
+    <div className="footer-dark">
         <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-md-3 item">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-md-3 item">
                
                         <ul>
                             <li><a href="#">Features</a></li>
@@ -16,7 +16,7 @@ const Footer = () => {
                             <li><a href="#">Pricing</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-6 col-md-3 item">
+                    <div className="col-sm-6 col-md-3 item">
                         
                         <ul>
                             <li><a href="#">Blog</a></li>
@@ -26,7 +26,7 @@ const Footer = () => {
                         </ul>
                     </div>
                     
-                    <div class="col-sm-6 col-md-3 item">
+                    <div className="col-sm-6 col-md-3 item">
                         
                         <ul>
                             <li><a href="#">About Us</a></li>
@@ -35,14 +35,14 @@ const Footer = () => {
                             <li> <a href="#">Login</a> </li>
                         </ul>
                     </div>
-                    <div class="col item social">
+                    <div className="col item social">
                     <a href="#"><i class="icon ion-social-facebook"></i></a>
                     <a href="#"><i class="icon ion-social-twitter"></i></a>
                     <a href="#"><i class="icon ion-social-linkedin"></i></a>
                     <a href="#"><i class="icon ion-social-instagram"></i></a>
                     </div>
                 </div>
-                <p class="copyright">Athene Design © 2020</p>
+                <p className="copyright">Athene Design © 2020</p>
             </div>
         </footer>
     </div>
