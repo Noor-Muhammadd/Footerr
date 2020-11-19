da3a17d40328bbd8ee9508e359f389e7eba54844
root <root@localhost>
2020-11-19 09:27:04 +0600

    publishing awesome pages 1605756424188

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <h2>Footer</h2>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+    
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+               
+                        <ul>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Enterprise </a></li>
+                            <li><a href="#">Pricing</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        
+                        <ul>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Help Center</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li> <a href="#">Status  </a> </li>
+                        </ul>
+                    </div>
+                    
+                    <div class="col-sm-6 col-md-3 item">
+                        
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Terms of Service</a></li>
+                            <li><a href="#">Security</a></li>
+                            <li> <a href="#">Login</a> </li>
+                        </ul>
+                    </div>
+                    <div class="col item social">
+                    <a href="#"><i class="icon ion-social-facebook"></i></a>
+                    <a href="#"><i class="icon ion-social-twitter"></i></a>
+                    <a href="#"><i class="icon ion-social-linkedin"></i></a>
+                    <a href="#"><i class="icon ion-social-instagram"></i></a>
+                    </div>
+                </div>
+                <p class="copyright">Athene Design © 2020</p>
+            </div>
+        </footer>
+    </div>
+    
+    );
+};
+
+export default Footer;
